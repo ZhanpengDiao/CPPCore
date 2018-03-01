214af43832511d1ee20beb132c58c2283dde1452
Zhanpeng DIAO <diaozhanpeng1015@gmail.com>
2018-03-02 00:31:31 +1100

    Initial Commit

---
+++ CPPCore/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPPCore
+//
+//  Created by 刁展鹏 on 2/3/18.
+//  Copyright © 2018 ZhanpengDiao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
