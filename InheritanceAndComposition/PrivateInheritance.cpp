8ca21e2b307769dc452278201aff54d1101d2c90
Zhanpeng Diao <diaozhanpeng1015@gmail.com>
2018-03-12 17:24:46 +1100

    private inheritance

---
+++ InheritanceAndComposition/PrivateInheritance.cpp
@@ -0,0 +1,23 @@
+
+// hide part of functions of base class
+
+class Pet {
+public:
+    char eat() {return 'a';}
+    int speak() {return 1;}
+    float sleep() const {return 2.0;}
+    float sleep(int) const {return 4.0;}
+};
+
+class Goldfish: Pet { // DEFALUT private inheritance
+public:
+    Pet::eat;
+    Pet::sleep; // all overloaded functions exposed
+};
+
+int main() {
+    Goldfish bob;
+    bob.eat();
+    bob.sleep();
+    // bob.speak(); // private functions 
+}
